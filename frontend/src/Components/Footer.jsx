fd980aed842699e785b2653a8137654dcfcef0a2
Asmit Samal <asmitsamal2@gmail.com>
2025-06-30 19:16:22 +0530

    Add navbar and footer

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-800 text-white p-3 text-center fixed bottom-0 w-full'>
+        <p>© 2025 e-Rto System. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-6 cursor-pointer text-white">
+        <li>Home</li>
+        <li>About</li>
+        <li>login</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
